f3735e5dd681254258d5f5659850139c61cf9c8a
LacombeLouis <lfmm.lacombe@gmail.com>
2024-09-17 15:48:13 +0200

    Refactor utils_openai.py to support optional parameters in get_embedding_score function

--- notebooks/utils_openai.py
+++ notebooks/utils_openai.py
@@ -3,10 +3,11 @@ import os
 # from openai import OpenAI
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from typing import List, Optional
+import numpy as np
+
 load_dotenv()
 
-
-# client = OpenAI()
 
 # You need to deploy your own embedding model as well as your own chat completion model
 embed_model = AzureOpenAIEmbedding(
@@ -20,3 +21,9 @@ embed_model = AzureOpenAIEmbedding(
 
 def get_embedding(text, embed_model=embed_model):
    return embed_model.get_text_embedding(text)
+
+
+def get_embedding_score(text: str, other_texts: List) -> dict:
+    text_emb = get_embedding(text)
+    cosine_similarities = np.dot(text_emb, np.array(other_texts).T)
+    return cosine_similarities
